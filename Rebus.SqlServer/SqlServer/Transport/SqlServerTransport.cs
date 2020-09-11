c8e3d467f17a57937fdb2e3a4486dd246896ea18
Rensvind <tommy@otyglad.net>
2020-09-11 13:51:22 +0200

    We need nocount off
    Without nocount off NPoco version handling won't work.

--- Rebus.SqlServer/SqlServer/Transport/SqlServerTransport.cs
+++ Rebus.SqlServer/SqlServer/Transport/SqlServerTransport.cs
@@ -375,7 +375,7 @@ IF EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = '{tableN
 	OUTPUT	deleted.[id] as [id],
 			deleted.[headers] as [headers],
 			deleted.[body] as [body]
-						
+    SET NOCOUNT OFF								
 						";
 
                 try
